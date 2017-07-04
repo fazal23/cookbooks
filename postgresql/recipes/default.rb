125bca78cfdf34fc23de24dda7db125da43231bf
fazal23 <jahidfazal.aptil@atmecs.com>
2017-07-04 09:01:55 -0400

    update

--- postgresql/recipes/default.rb
+++ postgresql/recipes/default.rb
@@ -5,7 +5,10 @@
 # Copyright (c) 2017 The Authors, All Rights Reserved.
 
 package 'postgresql-server' do 
-     notifies :run, 'execute[postgresql-init]'
+    notifies :run, 'execute[postgresql-init]'
+end
+package 'postgresql' do
+    action :install
 end
 
 execute 'postgresql-init' do
