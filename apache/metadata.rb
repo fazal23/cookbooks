146f19d3dc76e4856a05d642f07805d2935f0d55
fazal23 <jahidfazal.aptil@atmecs.com>
2017-07-03 21:32:42 -0400

    ye

---
+++ apache/metadata.rb
@@ -0,0 +1,16 @@
+name 'apache'
+maintainer 'Jahid'
+maintainer_email 'jahidfazalp7@gmail.com'
+license 'all_rights'
+description 'Installs/Configures apache'
+long_description 'Installs/Configures apache'
+version '1.2.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/apache/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/apache' if respond_to?(:source_url)
---
+++ httpd/recipes/default.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: httpd
+# Recipe:: default
+#
+# Copyright (c) 2017 The Authors, All Rights Reserved.
+
+package 'httpd' do
+  action :install
+end
+
+service 'httpd' do
+  action [ :enable, :start ]
+end
+
+cookbook_file "/var/www/html/index.html" do
+  source "index.html"
+  mode "0644"
+end
+
+
